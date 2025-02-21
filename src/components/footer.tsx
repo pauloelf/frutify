2e811e3c2713d3e7cf8b28d3a692ea9cd1851756
Paulo Sergio <contato.paulofws@gmail.com>
2025-02-21 18:26:53 -0300

    style: changed min-height

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,8 @@
-import { CherryIcon } from "lucide-react";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-emerald-950 shadow-sm h-25 -mb-25">
+    <footer className="bg-emerald-950 shadow-sm min-h-25 -mb-25">
       <div className="w-full max-w-screen-xl mx-auto px-4">
         <hr className="my-6 border-gray-400 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-gray-400 sm:text-center font-secondary">
