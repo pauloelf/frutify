e52be31d0cb3f172aceb6a41f3a7419cefc9352b
Paulo Sergio <contato.paulofws@gmail.com>
2025-03-01 18:38:28 -0300

    feat: add not-found page to path collection

---
+++ src/app/collection/not-found.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import bananaImage from '../../../public/banana.svg';
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <div className='flex-1 flex flex-col justify-center items-center px-8 py-2'>
+      <Image src={bananaImage} alt="Banana descascada" width={400} height={400} />
+      <h1 className='text-4xl text-emerald-700 font-primary mb-5'>Oops! Você escorregou na casca de banana!</h1>
+      <p className='text-xl text-emerald-500 font-secondary mb-10'>
+        A fruta que você está procurando escorregou para fora do nosso site. <br /> Volte para a{' '}
+        <Link href="/collection" className='underline text-emerald-600 hover:text-emerald-300'>Lista de Frutas</Link> e veja se encontra o que deseja!
+      </p>
+    </div>
+  );
+}
